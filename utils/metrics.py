baa0f6750d400e2a156a9c17aa79a84f38329b65
kwen <kwen.zou@outlook.com>
2025-04-20 10:51:40 +0800

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,5 @@
 import torch
-from typing import Dict, Any
+from typing import Dict
 from sklearn.metrics import roc_auc_score as auc
 
 
@@ -17,12 +17,15 @@ def calc_F1(TP: float,
     return F1
 
 def calc_AUC(mask_array: torch.Tensor,
-             predict_array: torch.Tensor):
+             predict_array: torch.Tensor,
+             reversed: bool = False):
     """
     compute AUC score
     Args:
         mask_array (torch.Tensor): ground truth, shape [h, w]
-        predict_array (torch.Tensor): predict array, shape [h, w]
+        predict_array (torch.Tensor): probability array for prediction, shape [h, w]
+        reversed (bool) :
+            if True, calculate AUC using inverted predicted values
     Return:
         auc metrics
     """
@@ -30,12 +33,18 @@ def calc_AUC(mask_array: torch.Tensor,
         "the shape of mask_array should be [h, w]"
     assert len(predict_array.shape) == 2, \
         "the shape of predict_array should be [h, w]"
+    assert torch.all((predict_array >= 0) & (predict_array <= 1)), \
+        "the value of predict_array should be [0, 1]"
+
+    if reversed:
+        predict_array = 1 - predict_array
+
     m = mask_array.flatten()
     p = predict_array.flatten()
     return auc(m, p)
 
 def cal_confusion_matrix(gt_mask: torch.Tensor,
-                         predict_mask: torch.Tensor) -> Dict[str, Any]:
+                         predict_mask: torch.Tensor) -> Dict[str, float]:
     """
     calculate  TP, TN, FP, FN for a image
     Args:
@@ -45,11 +54,11 @@ def cal_confusion_matrix(gt_mask: torch.Tensor,
         metrics (Dict) :  TP, TN, FP, FN metrics
     """
     assert predict_mask.shape == gt_mask.shape, \
-        "The shape of prediction is different from the mask"
+        "the shape of prediction is different from the mask"
     assert torch.all(torch.isin(predict_mask, torch.tensor([0, 1]))) ,\
-        "The value of predict should be 0 or 1"
+        "the value of predict should be 0 or 1"
     assert torch.all(torch.isin(gt_mask, torch.tensor([0, 1]))) ,\
-        "The value of ground truth should be 0 or 1"
+        "the value of ground truth should be 0 or 1"
     assert len(gt_mask.shape) == 2, \
         "the shape of mask should be [h, w]"
     assert len(predict_mask.shape) == 2, \
@@ -57,10 +66,10 @@ def cal_confusion_matrix(gt_mask: torch.Tensor,
 
 
     return {
-        "TP": torch.sum(predict_mask * gt_mask, dim=(0, 1)),
-        "TN": torch.sum((1 - predict_mask) * (1 - gt_mask), dim=(0, 1)),
-        "FP": torch.sum(predict_mask * (1 - gt_mask), dim=(0, 1)),
-        "FN": torch.sum((1 - predict_mask) * gt_mask, dim=(0, 1))
+        "TP": torch.sum(predict_mask * gt_mask, dim=(0, 1)).item(),
+        "TN": torch.sum((1 - predict_mask) * (1 - gt_mask), dim=(0, 1)).item(),
+        "FP": torch.sum(predict_mask * (1 - gt_mask), dim=(0, 1)).item(),
+        "FN": torch.sum((1 - predict_mask) * gt_mask, dim=(0, 1)).item()
     }
 
 
@@ -89,7 +98,8 @@ if __name__ == "__main__":
                                [1, 0, 0, 1, 1]])
     predict_array = torch.Tensor([[0.9, 0.8, 0.6, 0.1, 0.4],
                                   [0.7, 0.8, 0.7, 0.6, 0.9]])
-    print("auc value1", calc_AUC(mask_array, predict_array))
+    auc1 = calc_AUC(mask_array, predict_array)
+    print("auc1 value", auc1, type(auc1))
 
     mask_array = torch.Tensor([[1, 1, 1, 1],
                                [0, 0, 0, 0]])
