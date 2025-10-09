0a83febd0391cb75d1a67ce4cd23207e92362058
kwen <kwen.zou@outlook.com>
2025-10-09 17:12:23 +0800

    :art: 修改配置文件为 yaml

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-import tomli
+import yaml
 from pathlib import Path
 import pprint
 
-conf_path = Path(__file__).parent.joinpath("config.toml")
+conf_path = Path(__file__).parent.joinpath("config.yaml")
 
 with open(str(conf_path), 'rb') as f:
-	c = tomli.load(f)
+	c = yaml.safe_load(f)
 	pprint.pprint(c)
